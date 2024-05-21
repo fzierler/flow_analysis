5ae301bdaabbe9c378777aa5feb9c05815979819
Fabian Zierler <fabian.zierler@a1.net>
2024-05-21 11:49:08 +0100

    include  L=20 runs in analysis

--- flow_dial.jl
+++ flow_dial.jl
@@ -39,22 +39,37 @@ ensembles = [
     "measurements/Lt96Ls20beta6.5mf0.71mas1.01FUN",
 ]
 
-outputDIR = "outputDiaLTests"
+outputDIR = "outputDiaLTests"    
+W0=0.2815
 ensembles = [
     "measurementsTests/Lt64Ls16beta6.55mf0.69mas0.97FUN",
     "measurementsTests/Lt64Ls16beta6.55mf0.69mas0.99FUN",
-    "measurementsTests/Lt64Ls16beta6.55mf0.69mas1.01FUN",
+    #"measurementsTests/Lt64Ls16beta6.55mf0.69mas1.01FUN",
     "measurementsTests/Lt64Ls16beta6.55mf0.70mas0.97FUN",
     "measurementsTests/Lt64Ls16beta6.55mf0.70mas0.99FUN",
     #"measurementsTests/Lt64Ls16beta6.55mf0.70mas1.01FUN",
     "measurementsTests/Lt64Ls16beta6.55mf0.71mas0.97FUN",
-    "measurementsTests/Lt64Ls16beta6.55mf0.71mas0.99FUN",
+    #"measurementsTests/Lt64Ls16beta6.55mf0.71mas0.99FUN",
     #"measurementsTests/Lt64Ls16beta6.55mf0.71mas1.01FUN",
     #"measurementsTests/Lt96Ls32beta6.5mf0.71mas1.02FUN",
     #"measurementsTests/Lt96Ls32beta6.45mf0.718mas1.04FUN",
     #"measurementsTests/Lt96Ls40beta6.45mf0.722mas1.04FUN",
-    ]
-    
+]
+
+outputDIR = "outputDiaLTests"    
+W0=0.2815
+ensembles = [
+        "measurementsTests/Lt64Ls20beta6.55mf0.69mas0.97FUN",
+        "measurementsTests/Lt64Ls20beta6.55mf0.69mas0.99FUN",
+        "measurementsTests/Lt64Ls20beta6.55mf0.69mas1.01FUN",
+        "measurementsTests/Lt64Ls20beta6.55mf0.70mas0.97FUN",
+        "measurementsTests/Lt64Ls20beta6.55mf0.70mas0.99FUN",
+        "measurementsTests/Lt64Ls20beta6.55mf0.70mas1.01FUN",
+        "measurementsTests/Lt64Ls20beta6.55mf0.71mas0.97FUN",
+        "measurementsTests/Lt64Ls20beta6.55mf0.71mas0.99FUN",
+        "measurementsTests/Lt64Ls20beta6.55mf0.71mas1.01FUN",
+]
+
 path = "/home/fabian/Dokumente/DataDiaL/"
 path = "/home/fabian/Documents/DataDiaL/"
 
@@ -65,7 +80,7 @@ for ensemble in ensembles
     hirep_file = joinpath(path,"$ensemble/out/out_flow")
     output_file = joinpath(outputDIR,basename(ensemble)*"_flow")
     #run_flow_analysis(hirep_file,output_file)
-    run_flow_analysis(hirep_file,output_file;W0=0.1)
+    run_flow_analysis(hirep_file,output_file;W0=W0)
 
     # now get the plaquette
     plaq = plaquettes_log(hirep_file)
